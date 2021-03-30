5f57ee6e4803bffe91f410236b8b0343c8ecbaa8
KL-Huang <craint1001@gmail.com>
2021-03-31 07:42:27 +0800

    commit

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -18,8 +18,8 @@ int main()
     int value = 300; // frequency after confirm
     float value2 = 0.00;
     float period = 1/300;
-    int sample = 128;
-    float ADCdata[128];
+    int sample = 100;
+    float ADCdata[100];
 
     up.mode(PullNone);
     down.mode(PullNone);
@@ -47,7 +47,6 @@ int main()
             flag2 = 1;
             flag = 0;
         }
-    //    printf("flag2 = %d\n", flag2);
         uLCD.locate(0,(now % 3) * 2 + 1);
         uLCD.printf("O");
         for (int m = 0; m < 11; m++) 
@@ -55,25 +54,18 @@ int main()
                 uLCD.locate(0, m);
                 uLCD.printf(" ");
             }
-        if(flag2) {
-            period = 1.00/value;
-//            for (float j = period; j >= 0.00; j = j - period/1000.00)
-            for (float j = 1.0f; j >= 0.0f; j -= 0.01f) {
-                Aout = j;
-                wait_us(period * 100);
-            //    printf("period = %f\n", period);
-            //    printf("Aout = %f\n", 3.00 * (j/period));
-            //    ThisThread::sleep_for(10ms);
-            }
-            for (int i = 0; i < sample; i++) {
-                ADCdata[i] = Ain;
-                ThisThread::sleep_for(1000ms/sample);
-            }
-            for (int i = 0; i < sample; i++){
-                printf("%f\r\n", ADCdata[i]);
-                ThisThread::sleep_for(100ms);
-            }
-            flag2 = 0;
+        period = 1.00/value;
+        for (float j = 1.0f; j >= 0.0f; j -= 0.01f) {
+            int x = 0;
+            Aout = j;
+            wait_us(period * 100);
+            ADCdata[x] = Ain;
+//            ThisThread::sleep_for(1000ms/sample);
+//        }
+//        for (int i = 0; i < sample; i++){
+            printf("%f\r\n", ADCdata[x]);
+//            ThisThread::sleep_for(100ms);
+            x++;
         }
     }
 }
